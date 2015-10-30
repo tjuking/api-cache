c302b2f66fc8ffdbc032b55040f66c5129f18baa
lijin <tjuking@gmail.com>
2015-10-30 21:03:00 +0800

    v0.2

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # api-cache
-JavaScript对API数据的缓存组件
+
+JavaScript对API数据的缓存组件，可以利用localStorage来存储不常更新但经常访问的接口数据，通过设置缓存时间来控制实际发起请求的最小时间间隔。
+
+### 浏览器兼容性
+
+需要支持`localStorage`和`JSON`，请参考[http://caniuse.com/#search=localStorage](http://caniuse.com/#search=localStorage)、[http://caniuse.com/#feat=json](http://caniuse.com/#feat=json)
+
+### 依赖
+
+目前依赖[jQuery](http://jquery.com)，如果不希望依赖jQuery可以自己去实现函数`$.extend`方法
+
+### 使用示例
+
+```js
+    var one = new ApiCache({
+        name: "sidebar",
+        callback: function(ret){
+            console.log(ret);
+        },
+        ajax: function(){
+            return $.ajax({
+                url: "/user/sidebar",
+                dataType: "json"
+            });
+        }
+    });
+    one.start();
+```
--- api-cache.js
+++ api-cache.js
@@ -1,10 +1,10 @@
 /*
- * api-cache.js 0.1
- * https://github.com/tjuking/api-cache.js
+ * api-cache.js 0.2
+ * https://github.com/tjuking/api-cache
  * 可用于缓存接口数据到localStorage中
  */
 
-(function(global, factory){
+(function (global, factory) {
 
     "use strict";
 
@@ -18,19 +18,21 @@
         global.ApiCache = factory(jQuery, global);
     }
 
-})(typeof window !== "undefined" ? window : this, function($, window){
+})(typeof window !== "undefined" ? window : this, function ($, window) {
 
     "use strict";
 
     var storageSupport = supportStorage(); //localStorage支持性
     var jsonSupport = supportJson(); //JSON支持性
-    var noop = function(){}; //空函数
+    var noop = function () { //空函数
 
-    function ApiCache(options){
+    };
+
+    function ApiCache(options) {
         var defaultOptions = {
             prefix: "api_", //localStorage存储接口数据key值的前缀
             name: "", //localStorage存储接口数据key值后半部分，必填
-            timePrefix: "tapi_", //localStorage存储时间戳key值的前缀
+            timePrefix: "t_api_", //localStorage存储时间戳key值的前缀
             callback: noop, //callback是得到数据后的回调函数，必填
             ajax: noop, //ajax是发起请求的函数，需要返回Promise对象，必填
             time: 300000 //默认缓存五分钟
@@ -40,21 +42,21 @@
 
     ApiCache.prototype = {
 
-        constructor: ApiCache
+        constructor: ApiCache,
 
-        //检测支持性
-        check: function(){
-            return storageSupport && jsonSupport;
+        //所需支持性的检测，name不能为空
+        check: function () {
+            return storageSupport && jsonSupport && this.name;
         },
 
         //入口函数，无需请求接口返回值为true，否则返回false
-        start: function(){
-            if(this.check()){
+        start: function () {
+            if (this.check()) {
                 var now = +new Date();
                 var lastTime = localStorage.getItem(this.timePrefix + this.name);
                 var lastData = localStorage.getItem(this.prefix + this.name);
                 //存在数据且小于缓存时间
-                if(lastData && (now - lastTime < this.time)){
+                if (lastData && (now - lastTime < this.time)) {
                     lastData = JSON.parse(lastData);
                     this.callback.call(window, lastData);
                     return true;
@@ -65,30 +67,43 @@
         },
 
         //请求接口数据
-        sendAjax: function(){
-            this.ajax().done(this.callback).done(function(ret){
-                //请求到数据后需要存储数据和标记时间
-                if(this.check()){
-                    localStorage.setItem(this.prefix + this.name, JSON.stringify(ret));
-                    localStorage.setItem(this.timePrefix + this.name, +new Date());
-                }
+        sendAjax: function () {
+            var _this = this;
+            this.ajax().done(this.callback).done(function (ret) {
+                _this.set(ret); //请求到数据后需要存储数据和标记时间
             });
+        },
+
+        //设置存储的接口数据和时间戳
+        set: function (ret) {
+            if (this.check()) {
+                localStorage.setItem(this.prefix + this.name, JSON.stringify(ret));
+                localStorage.setItem(this.timePrefix + this.name, +new Date());
+            }
+        },
+
+        //清除存储的接口数据和时间戳
+        empty: function () {
+            if (this.check()) {
+                localStorage.removeItem(this.prefix + this.name);
+                localStorage.removeItem(this.timePrefix + this.name);
+            }
         }
     };
 
     //是否支持localStorage的特性检测
-    function supportStorage(){
-        try{
+    function supportStorage() {
+        try {
             localStorage.setItem("storageSupport", "1");
             localStorage.removeItem("storageSupport");
             return true;
-        }catch (e){
+        } catch (e) {
             return false;
         }
     }
 
     //是否支持JSON的检测
-    function supportJson(){
+    function supportJson() {
         return window.JSON && JSON.stringify && typeof JSON.stringify == "function" && JSON.parse && typeof JSON.parse == "function";
     }
 
